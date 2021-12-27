9ce214719fbefc0ebb47d87eb1994bd7c81bff33
Lyubov222 <lyubov.pan2000@mail.ru>
2021-12-28 00:21:21 +0700

    изменение

--- DresserUI/DresserUI/MainForm.cs
+++ DresserUI/DresserUI/MainForm.cs
@@ -19,7 +19,7 @@ namespace DresserUI
 
         private void buttonOpenDrawing_Click(object sender, EventArgs e)
         {
-            Drawing drawing = new Drawing(); 
+            Drawing drawing = new Drawing();  
             drawing.Show();
         } 
 
